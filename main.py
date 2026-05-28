4ab9a7b8aef000d28499588ac4d46dfb4f33f379
AARenor <arle.reidma@gmail.com>
2026-05-28 23:24:54 +0300

    chore: remove main.py - using index.py for Vercel

--- main.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from fastapi.staticfiles import StaticFiles
-
-from api.index import app
-
-# Serve static files for local dev (Vercel serves from public/ instead)
-if not os.environ.get("VERCEL"):
-    app.mount("/", StaticFiles(directory="static", html=True), name="static")
